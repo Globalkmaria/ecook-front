2cdb3104e084ec14ab896f235dfbda7c39934bd2
Maria <globalkmaria@gmail.com>
2024-11-20 00:01:29 +0900

    fix : header - text, style

--- src/app/components/layout/HomeHeader.tsx
+++ src/app/components/layout/HomeHeader.tsx
@@ -7,17 +7,17 @@ import { joinClassNames } from '@/utils/style';
 const libre = Libre_Bodoni({
   subsets: ['latin'],
   style: ['italic', 'normal'],
-  weight: ['700'],
+  weight: ['600', '400'],
 });
 
 function HomeHeader() {
   return (
     <header className={joinClassNames(style.header, libre.className)}>
-      <div>
-        <h1>Easy</h1>
-        <h1>& Delicious</h1>
-      </div>
-      <h1 className={style.header__title}>Recipes</h1>
+      <h1 className={style.title}>Discover the joy of cooking</h1>
+      <p className={style.description}>
+        Explore a world of delicious recipes, from timeless classics to modern
+        creations, crafted to inspire your next culinary adventure
+      </p>
     </header>
   );
 }
