1ff3760ad92e7291315b2024811685550c0bf89d
Maria <globalkmaria@gmail.com>
2024-12-05 04:37:41 +0900

    fix : build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 
 import '@/style/global.scss';
 import style from './style.module.scss';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Explore Delicious Recipes | E-COOK',
@@ -27,13 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <head>
+      <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Libre+Bodoni:wght@400..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
           rel='stylesheet'
         />
-        <link
+        {/* <link
           rel='apple-touch-icon'
           sizes='180x180'
           href='/img/apple-touch-icon.png'
@@ -73,8 +74,8 @@ export default function RootLayout({
           property='og:description'
           content='Got a recipe everyone loves? Share your creation with the RecipeHub community and inspire food lovers everywhere!'
         />
-        <meta property='og:site_name' content='E-COOK' />
-      </head>
+        <meta property='og:site_name' content='E-COOK' /> */}
+      </Head>
       <body>
         <div className={style.layout_container}>
           {children}
