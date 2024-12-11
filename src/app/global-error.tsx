d427fc47aae8cf765c5966e018fcd3941a2feef6
Maria <globalkmaria@gmail.com>
2024-12-11 18:12:39 +0900

    feat: add GlobalError component for handling errors in the application

---
+++ src/app/global-error.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import AnchorUnderline from '@/components/Anchor/AnchorUnderline';
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  console.error('Error:', error);
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+        <AnchorUnderline href='/'>Return to home</AnchorUnderline>
+      </body>
+    </html>
+  );
+}
