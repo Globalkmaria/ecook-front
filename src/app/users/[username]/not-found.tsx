288530ae055136a903b2207088ce5bcd6d940e46
Maria <globalkmaria@gmail.com>
2024-12-12 00:29:29 +0900

    fix: update not found messages to use template literals

--- src/app/users/[username]/not-found.tsx
+++ src/app/users/[username]/not-found.tsx
@@ -5,8 +5,8 @@ import AnchorUnderline from '@/components/Anchor/AnchorUnderline';
 export default async function NotFound() {
   return (
     <div className={style['not-found']}>
-      <h2>User not found 🥹</h2>
-      <p>Sorry, the user you're looking for doesn't exist.</p>
+      <h2>{`User not found 🥹`}</h2>
+      <p>{`Sorry, the user you're looking for doesn't exist.`}</p>
       <AnchorUnderline href='/'>Return to home</AnchorUnderline>
     </div>
   );
