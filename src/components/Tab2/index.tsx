87541598960d68126087ef9660168bc8fa3b22f0
Maria <globalkmaria@gmail.com>
2025-01-18 22:54:02 +0900

    refactor(Tab2 component): enhance  component structure with context for selected index

--- src/components/Tab2/index.tsx
+++ src/components/Tab2/index.tsx
@@ -1,29 +1,15 @@
 'use client';
 
+import { createContext, useContext, useState } from 'react';
 import style from './style.module.scss';
 
-export function Tab2({
-  children,
-  selected,
-  onClick,
-  className,
-}: {
-  children: React.ReactNode;
-  selected: boolean;
-  onClick: () => void;
-  className?: string;
-}) {
-  const selectedClass = selected ? style['tab--selected'] : '';
-  return (
-    <button
-      onClick={onClick}
-      type='button'
-      className={`${style.tab} ${selectedClass} ${className ?? ''}`}
-    >
-      {children}
-    </button>
-  );
-}
+const TabContext = createContext<
+  | {
+      selectedIndex: number;
+      setSelectedIndex: (index: number) => void;
+    }
+  | undefined
+>(undefined);
 
 export function TabsContainer2({
   children,
@@ -32,9 +18,44 @@ export function TabsContainer2({
   children: React.ReactNode;
   className?: string;
 }) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <div className={`${style['tabs-container']} ${className ?? ''}`}>
+    <TabContext.Provider value={{ selectedIndex, setSelectedIndex }}>
+      <div className={`${style['tabs-container']} ${className ?? ''}`}>
+        {children}
+      </div>
+    </TabContext.Provider>
+  );
+}
+
+interface Tab2Props {
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+  index: number;
+}
+
+export function Tab2({ children, onClick, className, index }: Tab2Props) {
+  const tabContext = useContext(TabContext);
+  if (!tabContext) {
+    throw new Error('Tab2 must be used within a TabsContainer2');
+  }
+
+  const selectedClass =
+    tabContext.selectedIndex === index ? style['tab--selected'] : '';
+
+  const handleClick = () => {
+    tabContext.setSelectedIndex(index);
+    onClick?.();
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      type='button'
+      className={`${style.tab} ${selectedClass} ${className ?? ''}`}
+    >
       {children}
-    </div>
+    </button>
   );
 }
