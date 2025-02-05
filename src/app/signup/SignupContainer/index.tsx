48fc5b3334b0558355e4ab3bfa0c7a3fdc3f84f1
Maria <globalkmaria@gmail.com>
2025-02-05 21:48:15 +0900

    feat(signup): add optional image upload helper text

--- src/app/signup/SignupContainer/index.tsx
+++ src/app/signup/SignupContainer/index.tsx
@@ -54,12 +54,15 @@ function SignupContainer() {
       <div className={style.container}>
         <h1>Sign up to E-COOK</h1>
         <div className={style.form}>
-          <div className={style['img-uploader']}>
-            <ImageUploader
-              onChange={onImgChange}
-              imgValue={form.img}
-              maxSizeKB={200}
-            />
+          <div>
+            <div className={style['img-uploader']}>
+              <ImageUploader
+                onChange={onImgChange}
+                imgValue={form.img}
+                maxSizeKB={200}
+              />
+            </div>
+            <span className={style['helper-text']}>* Image is optional</span>
           </div>
 
           <fieldset>
