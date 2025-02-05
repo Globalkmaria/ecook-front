10a721cba499874a7344d1939702ae8a37dbea6d
Maria <globalkmaria@gmail.com>
2025-02-06 01:01:15 +0900

    fix(search): type error

--- src/app/components/Search/helper.ts
+++ src/app/components/Search/helper.ts
@@ -1,4 +1,8 @@
-import { SEARCH_MENU_DEFAULT, SEARCH_MENU_VALUES } from '@/const/searchMenu';
+import {
+  SEARCH_MENU_DEFAULT,
+  SEARCH_MENU_VALUES,
+  SearchMenuValue,
+} from '@/const/searchMenu';
 import {
   lightSlugify,
   lightTrim,
@@ -8,5 +12,8 @@ import {
 export const getSearchQuery = (value: string | null) =>
   lightTrim(replaceHyphensWithSpaces(value ?? ''));
 
+const isSearchMenuType = (value: any): value is SearchMenuValue =>
+  SEARCH_MENU_VALUES.includes(value);
+
 export const getSearchMenuItem = (value: string | null) =>
-  value && SEARCH_MENU_VALUES.includes(value) ? value : SEARCH_MENU_DEFAULT;
+  value && isSearchMenuType(value) ? value : SEARCH_MENU_DEFAULT;
