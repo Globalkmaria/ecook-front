bd29277c09c31a7c68e196b5af31b997639e5811
Maria <globalkmaria@gmail.com>
2025-02-07 06:06:40 +0900

    feat : add useLogout, authError helper

--- src/services/utils/authError.ts
+++ src/services/utils/authError.ts
@@ -1,5 +1,12 @@
 export const UNAUTHORIZED_ERROR_CAUSE = 'UnauthorizedError';
 
-export const checkIsUnauthorizedError = (error: Error | null): boolean => {
-  return error?.cause === UNAUTHORIZED_ERROR_CAUSE;
-};
+export const checkIsUnauthorizedError = (error: Error | null): boolean =>
+  error?.cause === UNAUTHORIZED_ERROR_CAUSE;
+
+export const FORBIDDEN_ERROR_CAUSE = 'ForbiddenError';
+
+export const checkIsForbiddenError = (error: Error | null): boolean =>
+  error?.cause === FORBIDDEN_ERROR_CAUSE;
+
+export const checkIsAuthError = (error: Error | null): boolean =>
+  checkIsUnauthorizedError(error) || checkIsForbiddenError(error);
