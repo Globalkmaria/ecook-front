234696196b8408accfc9b88030fe67c480dbea5a
Maria <globalkmaria@gmail.com>
2025-02-06 22:58:04 +0900

    feat (bookmark page) : add SavedRecipes component

--- src/app/sn/bookmarks/SavedRecipes/index.tsx
+++ src/app/sn/bookmarks/SavedRecipes/index.tsx
@@ -1,5 +1,45 @@
+'use client';
+
+import style from './style.module.scss';
+
+import { RecipeSimple } from '@/services/recipe/type';
+
+import RecipeImgAndInfoCard from '@/app/components/RecipeImgAndInfoCard';
+
+import { getCountText } from './helper';
+
+const recipes = {
+  count: 0,
+  recipes: [],
+};
+
 function SavedRecipes() {
-  return <div>Saved Recipes</div>;
+  const countText = getCountText(recipes.count);
+
+  return (
+    <section>
+      <div className={style['header']}>
+        <h2 className={style['title']}>Saved Recipes</h2>
+        <span className={style['count']}>{countText}</span>
+      </div>
+      <div>
+        <List recipes={recipes.recipes} />
+      </div>
+    </section>
+  );
 }
 
 export default SavedRecipes;
+
+function List({ recipes }: { recipes: RecipeSimple[] }) {
+  if (!recipes.length) return null;
+  return (
+    <ul className={style['list']}>
+      {recipes.map((recipe) => (
+        <li key={recipe.id}>
+          <RecipeImgAndInfoCard recipe={recipe} />
+        </li>
+      ))}
+    </ul>
+  );
+}
