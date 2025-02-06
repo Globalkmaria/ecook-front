cbb29b0d6b6500937aaf6449d8cc0d58ca247af4
Maria <globalkmaria@gmail.com>
2025-02-07 08:44:50 +0900

    fix: clear query cache on logout

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import { useRouter } from 'next/navigation';
 
 import { useClientStore } from '@/providers/client-store-provider';
 
 import { LOGIN_LINK } from '@/helpers/links';
+import { useQueryClient } from '@tanstack/react-query';
 
 function useLogout() {
   const router = useRouter();
   const resetUser = useClientStore((state) => state.resetUser);
+  const query = useQueryClient();
 
   const logout = () => {
     Promise.resolve().then(() => {
       resetUser();
       router.push(LOGIN_LINK);
+      query.clear();
     });
   };
 
