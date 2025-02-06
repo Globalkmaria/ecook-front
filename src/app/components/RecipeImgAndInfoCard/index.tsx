e97489b7527f36337c476a0dd766200b61eaeed1
Maria <globalkmaria@gmail.com>
2025-02-06 22:40:55 +0900

    feat : add `<RecipeImgAndInfoCard/>` component

---
+++ src/app/components/RecipeImgAndInfoCard/index.tsx
@@ -0,0 +1,57 @@
+import style from './style.module.scss';
+
+import Link from 'next/link';
+
+import { RecipeSimple } from '@/services/recipe/type';
+
+import { formatTime } from '@/utils/time';
+
+import { getRecipeLink } from '@/helpers/links';
+
+import Avatar from '@/components/Avatar';
+import Chip2 from '@/components/Chip2';
+import CustomImage from '@/components/CustomImage';
+
+interface Props {
+  recipe: RecipeSimple;
+}
+
+function RecipeImgAndInfoCard({ recipe }: Props) {
+  const time = formatTime({ hours: recipe.hours, minutes: recipe.minutes });
+  const link = getRecipeLink(recipe.key);
+
+  return (
+    <div className={style['wrapper']}>
+      <Link href={link} className={style.card}>
+        <div className={style['top-overlay']}>
+          <div className={style['avatar']}>
+            <Avatar user={recipe.user} />
+          </div>
+        </div>
+        <div className={style['img-box']}>
+          <div className={style['img-wrapper']}>
+            <CustomImage
+              loadingClassName={style['img-placeholder']}
+              imgClassName={style.img}
+              src={recipe.img}
+              alt={recipe.name}
+              fill
+            />
+          </div>
+        </div>
+
+        <div className={style.info}>
+          <span className={style.title}>{recipe.name}</span>
+          <span className={style.time}>{time}</span>
+
+          <Chip2.Container>
+            {recipe.tags.map((tag) => (
+              <Chip2.Chip key={tag.id}>{tag.name}</Chip2.Chip>
+            ))}
+          </Chip2.Container>
+        </div>
+      </Link>
+    </div>
+  );
+}
+export default RecipeImgAndInfoCard;
