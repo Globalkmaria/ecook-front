5624b119df3dac21f2babac0defe8dd686129f7c
Maria <globalkmaria@gmail.com>
2025-02-07 23:30:00 +0900

    refactor(new recipe) : implement useCreateRecipe hook for recipe submission

--- src/queries/hooks/index.ts
+++ src/queries/hooks/index.ts
@@ -2,3 +2,4 @@ export { useAddBookmarkMutation } from './useAddBookmarkMutation';
 export { useDeleteRecipeMutation } from './useDeleteRecipeMutation';
 export { useEditRecipeMutation } from './useEditRecipeMutation';
 export { useRemoveBookmarkMutation } from './useRemoveBookmarkMutation';
+export { useCreateRecipe } from './useCreateRecipe';
---
+++ src/queries/hooks/useCreateRecipe.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { useClientStore } from '@/providers/client-store-provider';
+
+import { isUnauthorizedResponse } from '@/services/utils/authError';
+import { createRecipe } from '@/services/recipes';
+
+import {
+  generateProductListQueryKey,
+  generateRecipeListQueryKey,
+  generateUserProfileQueryKey,
+} from '@/queries/helpers';
+
+import useLogout from '@/hooks/useLogout';
+
+import { getRecipeLink, LOGIN_LINK } from '@/helpers/links';
+
+export const useCreateRecipe = () => {
+  const logout = useLogout();
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const username = useClientStore((state) => state.user.username);
+
+  const result = useMutation({
+    mutationFn: async (data: FormData) => {
+      const response = await createRecipe(data);
+      if (response.ok) return response.data;
+
+      if (isUnauthorizedResponse(response.res)) {
+        logout();
+        throw new Error('Please login again to use this service');
+      }
+
+      throw new Error('Failed to submit recipe');
+    },
+    onSuccess: async (data) => {
+      if (!username) {
+        alert('Please login to edit recipe');
+        router.replace(LOGIN_LINK);
+        return;
+      }
+
+      queryClient.invalidateQueries({
+        queryKey: generateRecipeListQueryKey({
+          query: 'username',
+          type: username,
+        }),
+      });
+      queryClient.invalidateQueries({
+        queryKey: generateUserProfileQueryKey(username),
+      });
+      queryClient.invalidateQueries({
+        queryKey: generateProductListQueryKey({
+          type: 'username',
+          query: username,
+        }),
+      });
+
+      router.replace(getRecipeLink(data.key));
+    },
+    onError: (error) => alert(error.message),
+    retry: 3,
+    retryDelay: 5000, // 5 seconds
+  });
+
+  return result;
+};
