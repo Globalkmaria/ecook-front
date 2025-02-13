073b1ca75732ea4962e20c5e14b5509f07b65eca
Maria <globalkmaria@gmail.com>
2025-02-14 01:04:36 +0900

    fix : no ssr client component to use 'dynamic'

---
+++ src/app/components/layout/Nav/NavRightButtons/NavRightsButtonsContent.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { useRef } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useShallow } from 'zustand/shallow';
+import { useQueryClient } from '@tanstack/react-query';
+
+import style from './style.module.scss';
+
+import { useClientStore } from '@/providers/client-store-provider';
+
+import { logout } from '@/services/requests/auth';
+
+import {
+  NEW_RECIPE_LINK,
+  getUserLink,
+  HOME_LINK,
+  LOGIN_LINK,
+  SIGNUP_LINK,
+  BOOKMARKS_LINK,
+} from '@/helpers/links';
+
+import { AvatarImg } from '@/components/Avatar';
+import Anchor from '@/components/Anchor';
+import { Dropbox, DropboxItem, DropboxWrapper } from '@/components/Dropbox';
+import Icon from '@/components/Icon';
+
+function NavRightButtons() {
+  const user = useClientStore(useShallow((state) => state.user));
+
+  if (!user.isLoggedIn) return <NotLoggedInMenu />;
+
+  return <LoggedInMenu />;
+}
+
+export default NavRightButtons;
+
+function LoggedInMenu() {
+  const query = useQueryClient();
+  const router = useRouter();
+  const ref = useRef<HTMLDivElement>(null);
+  const [resetUser, user] = useClientStore(
+    useShallow((state) => [state.resetUser, state.user]),
+  );
+
+  const userImgInfo = {
+    username: user.username ?? '',
+    img: user.img,
+  };
+
+  const onLogout = async () => {
+    logout();
+    resetUser();
+    Promise.resolve().then(() => query.clear());
+
+    router.push(HOME_LINK);
+  };
+
+  return (
+    <div className={style['right-buttons']}>
+      <Anchor variant='secondary' href={BOOKMARKS_LINK}>
+        <Icon icon='book' />
+      </Anchor>
+      <Anchor href={NEW_RECIPE_LINK} className={style.new}>
+        <Icon icon='add' className={style['new__icon']} />
+        <span className={style['new__text']}>New recipes</span>
+      </Anchor>
+      <div className={style.profile}>
+        <Link href={getUserLink(user.username ?? '')}>
+          <AvatarImg user={userImgInfo} size={48} hoverable />
+        </Link>
+        <DropboxWrapper ref={ref}>
+          <Dropbox className={style['profile-dropbox']} containerRef={ref}>
+            <DropboxItem onClick={onLogout}>Logout</DropboxItem>
+          </Dropbox>
+        </DropboxWrapper>
+      </div>
+    </div>
+  );
+}
+
+function NotLoggedInMenu() {
+  return (
+    <div className={style['right-buttons']}>
+      <Anchor variant='secondary' href={BOOKMARKS_LINK}>
+        <Icon icon='book' />
+      </Anchor>
+      <Anchor variant='secondary' href={SIGNUP_LINK}>
+        Sign up
+      </Anchor>
+      <Anchor href={LOGIN_LINK}>Login</Anchor>
+    </div>
+  );
+}
--- src/app/sn/bookmarks/SavedRecipes/index.tsx
+++ src/app/sn/bookmarks/SavedRecipes/index.tsx
@@ -1,149 +1,20 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { useShallow } from 'zustand/shallow';
+import dynamic from 'next/dynamic';
 
 import style from './style.module.scss';
 
-import { RecipeSimple } from '@/services/requests/recipe/type';
-import { isAuthError } from '@/services/utils/authError';
-
-import { getBookmarkedRecipes } from '@/stores/slices/helper';
-
-import { useClientStore } from '@/providers/client-store-provider';
-
-import {
-  userBookmarkedRecipesOptions,
-  recipesBatchOptions,
-} from '@/queries/options';
-
-import useLogout from '@/hooks/useLogout';
-
-import RecipeImgAndInfoCard from '@/app/components/common/RecipeImgAndInfoCard';
-
-import { getCountText } from './helper';
+const SavedRecipeList = dynamic(() => import('./SavedRecipeList'), {
+  ssr: false,
+});
 
 function SavedRecipes() {
   return (
     <section>
       <h2 className={style['title']}>Saved Recipes</h2>
-      <Recipes />
+      <SavedRecipeList />
     </section>
   );
 }
 
 export default SavedRecipes;
-
-function Recipes() {
-  const isLoggedIn = useClientStore((state) => state.user.isLoggedIn);
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) return null;
-
-  return isLoggedIn ? <LoginContent /> : <NotLoggedInContent />;
-}
-
-function NotLoggedInContent() {
-  const bookmarks = useClientStore(useShallow((state) => state.bookmarks));
-  const recipeKeys = getBookmarkedRecipes(bookmarks);
-  const enabled = recipeKeys.length > 0;
-
-  const { data, error, isLoading } = useQuery(
-    recipesBatchOptions({
-      query: recipeKeys,
-      type: 'keys',
-      enabled,
-    }),
-  );
-
-  return <Content data={data} isLoading={isLoading} error={error} />;
-}
-
-function LoginContent() {
-  const username = useClientStore((state) => state.user?.username);
-  const logout = useLogout();
-  const { data, error, isLoading } = useQuery(
-    userBookmarkedRecipesOptions({
-      username: username ?? '',
-      enabled: !!username,
-    }),
-  );
-
-  if (isAuthError(error)) {
-    logout();
-    return;
-  }
-
-  return <Content data={data} isLoading={isLoading} error={error} />;
-}
-
-interface ContentProps {
-  data?: {
-    search: RecipeSimple[];
-    recommend: RecipeSimple[];
-  };
-  isLoading: boolean;
-  error: Error | null;
-}
-
-function Content({ data, isLoading, error }: ContentProps) {
-  if (isLoading) return <ListSkeleton />;
-
-  if (error) return <ErrorMessage />;
-
-  if (!data || !data.search.length) return <NoContent />;
-
-  const countText = getCountText((data?.search || []).length);
-
-  return (
-    <div>
-      <span className={style['text']}>{countText}</span>
-      <List recipes={data?.search ?? []} />
-    </div>
-  );
-}
-
-function List({ recipes }: { recipes: RecipeSimple[] }) {
-  if (!recipes.length) return null;
-
-  return (
-    <ul className={style['list']}>
-      {recipes.map((recipe) => (
-        <li key={recipe.id}>
-          <RecipeImgAndInfoCard.Card recipe={recipe} />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function ListSkeleton() {
-  const list = Array.from({ length: 4 });
-  return (
-    <ul className={style['list']}>
-      {list.map((_, index) => (
-        <li key={index}>
-          <RecipeImgAndInfoCard.Skeleton />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function NoContent() {
-  return (
-    <div className={style['text']}>
-      <p>No saved recipes yet.</p>
-      <p>Save recipes by clicking the bookmark icon on the recipe page</p>
-    </div>
-  );
-}
-
-function ErrorMessage() {
-  return <div>Something went wrong. Please try again later.</div>;
-}
