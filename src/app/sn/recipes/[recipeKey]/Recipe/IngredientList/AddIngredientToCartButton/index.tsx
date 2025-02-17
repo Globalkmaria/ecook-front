5376ced0412a49a2bcc3c9ef0e36f965bb42489c
Maria <globalkmaria@gmail.com>
2025-02-17 11:01:08 +0900

    feat(cart): integrate create cart item mutation for logged-in users

--- src/app/sn/recipes/[recipeKey]/Recipe/IngredientList/AddIngredientToCartButton/index.tsx
+++ src/app/sn/recipes/[recipeKey]/Recipe/IngredientList/AddIngredientToCartButton/index.tsx
@@ -6,6 +6,8 @@ import style from './style.module.scss';
 
 import { useClientStore } from '@/providers/client-store-provider';
 
+import { useCreateCartItemMutation } from '@/queries/hooks/carts/useCreateCartItemMutation';
+
 import Icon from '@/components/Icon';
 
 interface Props {
@@ -14,10 +16,23 @@ interface Props {
 }
 
 function AddIngredientToCartButton({ ingredientKey, productKey }: Props) {
-  const [addIngredientToCart, addProductToCart] = useClientStore(
-    useShallow((state) => [state.addIngredientToCart, state.addProductToCart]),
+  const [addIngredientToCart, addProductToCart, isLoggedIn] = useClientStore(
+    useShallow((state) => [
+      state.addIngredientToCart,
+      state.addProductToCart,
+      state.user.isLoggedIn,
+    ]),
   );
+  const { mutate } = useCreateCartItemMutation();
   const onAddToCart = () => {
+    if (isLoggedIn) {
+      mutate({
+        ingredientKey,
+        productKey,
+      });
+      return;
+    }
+
     if (productKey) {
       addProductToCart(ingredientKey, productKey);
     } else {
