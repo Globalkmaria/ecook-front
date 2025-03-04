a474c29f346e7db11e9849469755bf5f8392219d
Song Hyun Kim <globalkmaria@gmail.com>
2025-03-05 05:59:09 +0900

    feat(PageModalWrapper): this modal wrapper is used when making interrupt route modal.

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,3 +1,4 @@
 export { default as Modal } from './Modal';
 export { default as Modal2 } from './Modal2';
 export { default as ModalWrapper } from './ModalWrapper';
+export { default as PageModalWrapper } from './PageModalWrapper';
