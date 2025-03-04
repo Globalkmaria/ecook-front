c1b39ed3b09d915ceb4d212a37281a2108039dc1
Song Hyun Kim <globalkmaria@gmail.com>
2025-03-05 05:34:12 +0900

    feat(Chip) : add type prop

--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -1,20 +1,23 @@
 import { joinClassNames } from '@/utils/style';
 import style from './style.module.scss';
 
-function Chip({
-  children,
-  className,
-  border,
-}: {
+type ChipType = 'default' | 'warning' | 'info' | 'success';
+
+export interface ChipProps {
   children: React.ReactNode;
   className?: string;
   border?: boolean;
-}) {
+  type?: ChipType;
+}
+
+function Chip({ children, className, border, type = 'default' }: ChipProps) {
   const borderClassName = border ? style['chip--border'] : '';
+  const typeClassName = style[`chip--${type}`];
 
   const joinedClassName = joinClassNames(
     style.chip,
     borderClassName,
+    typeClassName,
     className || '',
   );
 
