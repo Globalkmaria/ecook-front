d1e1ec3e8b52115832d2175d451b75c93cb1f82a
Song Hyun Kim <globalkmaria@gmail.com>
2025-03-08 12:33:51 +0900

    fix(eslint): small changes

--- src/app/components/common/Search/helper.ts
+++ src/app/components/common/Search/helper.ts
@@ -9,8 +9,8 @@ import {
 export const getSearchQuery = (value: string | null) =>
   lightTrim(replaceHyphensWithSpaces(value ?? ''));
 
-const isSearchMenuType = (value: any): value is SearchMenuValue =>
-  SEARCH_MENU_VALUES.includes(value);
+const isSearchMenuType = (value: string): value is SearchMenuValue =>
+  SEARCH_MENU_VALUES.includes(value as SearchMenuValue);
 
 export const getSearchMenuItem = (value: string | null) =>
   value && isSearchMenuType(value) ? value : SEARCH_MENU_DEFAULT;
--- src/services/requests/recipes/helper.ts
+++ src/services/requests/recipes/helper.ts
@@ -10,8 +10,10 @@ export const SEARCH_TYPES = Object.values(SEARCH_TYPES_KEYS);
 
 export type RecipesSearchType = (typeof SEARCH_TYPES)[number];
 
-export const checkSearchType = (type: any): type is RecipesSearchType =>
-  SEARCH_TYPES.includes(type);
+export const checkSearchType = (
+  type: string | undefined,
+): type is RecipesSearchType =>
+  SEARCH_TYPES.includes(type as RecipesSearchType);
 
 export const RECIPES_BATCH_TYPES_KEYS = {
   KEYS: 'keys',
@@ -20,6 +22,3 @@ export const RECIPES_BATCH_TYPES_KEYS = {
 export const RECIPES_BATCH_TYPES = Object.values(RECIPES_BATCH_TYPES_KEYS);
 
 export type RecipesBatchType = (typeof RECIPES_BATCH_TYPES)[number];
-
-export const checkBatchType = (type: any): type is RecipesBatchType =>
-  RECIPES_BATCH_TYPES.includes(type);
