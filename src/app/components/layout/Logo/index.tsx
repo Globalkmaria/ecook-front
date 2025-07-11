469d46f44699bd3da23c865e0ff1d2e5ccfe09c3
Song Hyun Kim <globalkmaria@gmail.com>
2025-07-12 01:19:06 +0900

    fix: update default theme in Logo component from light to dark

--- src/app/components/layout/Logo/index.tsx
+++ src/app/components/layout/Logo/index.tsx
@@ -8,7 +8,7 @@ interface LogoProps {
   theme?: 'light' | 'dark';
 }
 
-function Logo({ theme = 'light' }: LogoProps) {
+function Logo({ theme = 'dark' }: LogoProps) {
   const titleClassName = `${style.title} ${theme === 'light' ? style.light : style.dark}`;
   return (
     <Link href={HOME_LINK} className={style.logo}>
